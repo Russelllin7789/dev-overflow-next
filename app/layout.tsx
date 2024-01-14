00fef7d878a26c36c221e86f43cf3c26d666f1f9
Russell Lin <superbr7789@gmail.com>
2024-01-14 16:49:06 +0800

    set layout settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,18 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export const metadata: Metadata = {
   title: "DevOverflow",
   description:
@@ -20,9 +32,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
