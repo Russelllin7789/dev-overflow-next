73bb22cc1d557147a39386d3450ae5dfbb479df8
Russell Lin <superbr7789@gmail.com>
2024-01-19 19:58:23 +0800

    add badge for tag component

--- components/shared/tag/Tag.tsx
+++ components/shared/tag/Tag.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 
 const Tag = ({
@@ -10,9 +11,9 @@ const Tag = ({
 }) => {
   return (
     <Link href="/" className="flex items-center justify-between">
-      <p className="btn text-light400_light500 subtle-medium rounded-[6px] px-4 py-2 uppercase">
+      <Badge className="btn text-light400_light500 subtle-medium rounded-[6px] px-4 py-2 uppercase">
         {tagContent}
-      </p>
+      </Badge>
       {tagNumber && (
         <p className="body-medium text-dark300_light900 small-medium">
           {tagNumber}
