b99e65b152156686df4211be3aa067811e9c5da7
Russell Lin <superbr7789@gmail.com>
2024-01-28 20:16:07 +0800

    add basic server action setup with mongodb

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -18,6 +18,7 @@ import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { createQuestion } from "@/lib/actions/question.action";
 
 const type: any = "create" || "edit";
 
@@ -36,11 +37,12 @@ const Question = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+  async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     setIsSubmitting(true);
 
     try {
       // make an async call to api
+      await createQuestion({});
     } catch (error) {
     } finally {
       setIsSubmitting(false);
@@ -129,6 +131,8 @@ const Question = () => {
                     // @ts-ignore
                     editorRef.current = editor;
                   }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
                   initialValue=""
                   init={{
                     height: 350,
