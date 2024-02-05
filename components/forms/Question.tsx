533a02e412a31885383108967bbbf5d215d355ca
Russell Lin <superbr7789@gmail.com>
2024-02-05 21:41:19 +0800

    correct the path params with usePathname's value

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -54,7 +54,7 @@ const Question = ({ mongoUserId }: Props) => {
         content: values.explanation,
         tags: values.tags,
         author: JSON.parse(mongoUserId),
-        path: "/",
+        path: pathname,
       });
 
       router.push("/");
