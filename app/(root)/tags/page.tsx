ef3bab5c33ef40af5090d41194188a5827443221
Russell Lin <superbr7789@gmail.com>
2024-02-11 19:53:54 +0800

    fulfill the whole tag page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -3,6 +3,7 @@ import { TagFilters } from "@/constants/filters";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import Filter from "@/components/shared/Filter";
 import { getAllTags } from "@/lib/actions/tag.action";
+import NoResult from "@/components/shared/NoResult";
 
 const page = async () => {
   const result = await getAllTags({});
@@ -13,7 +14,7 @@ const page = async () => {
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
-          route="/community"
+          route="/tags"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search by tag name..."
@@ -27,17 +28,35 @@ const page = async () => {
 
       <section className="mt-12 flex flex-wrap gap-4">
         {result.tags.length >= 0 ? (
-          result.tags.map((tag) => <div key={tag.name}>{tag.name}</div>)
-        ) : (
-          <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
-            <p>No tags yet</p>
+          result.tags.map((tag) => (
             <Link
-              href="/ask-question"
-              className="mt-2 font-bold text-accent-blue"
+              href={`tags/${tag._id}`}
+              key={tag._id}
+              className="shadow-light100_darknone"
             >
-              Create question to be the very first!
+              <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+                <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.questions.length}+
+                  </span>{" "}
+                  Questions
+                </p>
+              </article>
             </Link>
-          </div>
+          ))
+        ) : (
+          <NoResult
+            title="No Tags Found"
+            description="It looks like there are no tags found."
+            link="/ask-question"
+            linkTitle="Ask a question"
+          />
         )}
       </section>
     </>
